6586b25ae2abcc2f568abdcdcbc5c8f8c4778d47
Ty Raino <mstymetris@yahoo.com>
2020-03-27 23:55:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,9 @@
-WIN_COMBINATIONS.detect do |combo| 
-       if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" 
+WIN_COMBINATIONS.detect do |combo|
+       if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
          return "X"
        elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
          return "O"
-       else 
+       else
          nil
        end
  end
