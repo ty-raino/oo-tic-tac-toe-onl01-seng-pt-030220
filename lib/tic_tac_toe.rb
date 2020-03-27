8e64b7545d7a176488b585afefdd25006c738a88
Ty Raino <mstymetris@yahoo.com>
2020-03-27 23:47:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,12 @@ class TicTacToe
   WIN_COMBINATIONS = [
     [0, 1, 2],
     [3, 4 ,5],
-    [6, 7, 8]
+    [6, 7, 8],
+    [0, 3, 5],
+    [1, 7, 2],
+    [6, 4, 8],
+    [5, 8, 0]
+    [7, 3, 1]
   ]
 
   def initialize(board = nil)
